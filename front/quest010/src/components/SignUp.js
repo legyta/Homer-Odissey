86c0306d8575e8b2c0f2717c4b355ad3f87238ba
Ligita <legyta@gmail.com>
2020-03-23 17:21:50 +0100

    quest020

--- front/quest010/src/components/SignUp.js
+++ front/quest010/src/components/SignUp.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 class SignUp extends Component {
   state = {
+    name: "James",
+    lastname: "Bond",
+    password: "monPassw0rd",
+    email: "mon@email.com",
     userInput: "test@test.com"
   };
 
@@ -10,18 +14,42 @@ class SignUp extends Component {
     this.setState({ userInput: email });
   };
 
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(JSON.stringify(this.state));
+  };
+
   render() {
     return (
       <div className="form-email">
-        <h1>{this.state.userInput} </h1>
-        <label htmlFor="exampleEmail">enter email here</label>
-        <input
-          type="email"
-          name="email"
-          className="form-emailPlace"
-          value={this.state.userInput}
-          onChange={this.updateEmailField}
-        />
+        <h1>{JSON.stringify(this.state, 1, 1)} </h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="firstname" placeholder="enter your name" />
+          <label htmlFor="lastname">Lastname</label>
+          <input type="text" name="lastname" placeholder="enter your surname" />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="enter your password"
+          />
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            name="password-conf"
+            placeholder="confirm your password"
+          />
+          <label htmlFor="email">enter email here</label>
+          <input
+            type="email"
+            name="email"
+            className="signup-email"
+            value={this.state.userInput}
+            onChange={this.updateEmailField}
+          />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }

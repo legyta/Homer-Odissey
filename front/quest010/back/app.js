733938b59d3208ca37468e5b3032cd2ae6eb67c6
Ligita <legyta@gmail.com>
2020-03-25 00:38:48 +0100

    quest030

--- front/quest010/back/app.js
+++ front/quest010/back/app.js
@@ -1,16 +1,18 @@
 // declare all the necessary libraries
 const http = require("http");
-const path = require("path");
+//const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
+const authRouter = require("./routes/auth/auth");
 
 // set up the application
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
+app.use("/auth", authRouter);
 
 // implement the API part
 app.get("/", (req, res) => {
@@ -23,7 +25,12 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+//posting
+app.post();
+
 // launch the node server
 let server = app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port " + server.address().port);
 });
+
+module.exports = app;
---
+++ front/quest010/back/routes/auth/auth.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const authRouter = express.Router();
+
+authRouter.post("/signup", function(req, res, next) {
+  res.send("I am in POST signup");
+});
+
+module.exports = authRouter;

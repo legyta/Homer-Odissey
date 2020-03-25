b70cd9617ed2cb10cedfd48a95183d1788eca8fd
Ligita <legyta@gmail.com>
2020-03-25 01:42:40 +0100

    quest040

--- front/quest010/back/routes/auth/auth.js
+++ front/quest010/back/routes/auth/auth.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const authRouter = express.Router();
+const connection = require("../../helpers/db.js");
+
+// authRouter.post("/signup", function(req, res, next) {
+//   res.send("I am in POST signup");
+// });
 
 authRouter.post("/signup", function(req, res, next) {
-  res.send("I am in POST signup");
-});
-
-var post = { id: 1, title: "Hello MySQL" };
-var query = connection.query("INSERT INTO posts SET ?", post, function(
-  error,
-  results,
-  fields
-) {
-  if (error) res.status(500).end();
-
-  res.end();
+  const formFill = req.body;
+  connection.query("INSERT INTO posts SET ?", formFill, (error, results) => {
+    if (error) {
+      results.status(500).send(error);
+      results.end();
+    } else {
+      results.status(200).json(results);
+      results.end();
+    }
+  });
 });
 
 module.exports = authRouter;
